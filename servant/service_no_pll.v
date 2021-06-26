951f5f3fae54793ccb2ba0b8c62ba633d981aa60
Dave Dribin <dave-vcs@dribin.org>
2021-06-26 18:02:50 -0500

    Go Board: Remove external reset and add a counter to hold reset for 100ms at startup

--- servant/service_no_pll.v
+++ servant/service_no_pll.v
@@ -1,17 +1,22 @@
 `default_nettype none
 module service
   (input wire  i_clk,
-   input wire  i_rst,
    output wire q);
 
    parameter memfile = "zephyr_hello.hex";
    parameter memsize = 8192;
 
-   reg            rst;
+   reg [20:0]     rst_count;
    reg            rst_r;
 
-   always @(posedge i_clk)
-     {rst_r, rst} <= {rst, i_rst};
+  always @(posedge i_clk) begin
+    if (rst_count < 21'd2_000_000) begin
+      rst_r <= 1;
+      rst_count <= rst_count + 21'd1;
+    end else begin
+      rst_r <= 0;
+    end
+   end
 
    servant
      #(.memfile (memfile),
